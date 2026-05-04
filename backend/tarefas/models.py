c15433cf211616aca002adc38888dd8d43fe437b
Bernardo <bernardodspr@gmail.com>
2026-05-04 19:56:34 -0300

    Aula 4 - API funcionando

--- backend/tarefas/models.py
+++ backend/tarefas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Tarefa(models.Model):
+    STATUS_CHOICES = [
+        ('ABERTA', 'Aberta'),
+        ('CONCLUIDA', 'Concluída'),
+    ]
+
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='ABERTA')
+
+    def __str__(self):
+        return self.titulo
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from .models import Tarefa
+
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all().values()
+    return JsonResponse(list(tarefas), safe=False)
